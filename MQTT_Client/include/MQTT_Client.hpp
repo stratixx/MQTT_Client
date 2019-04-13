ae5d23830340e784aa270cbd69472cd4a1eaf4a7
stratixx <darnokusex@wp.pl>
2019-04-13 11:28:39 +0200

    boost working with cmake on Windows

--- MQTT_Client/include/MQTT_Client.hpp
+++ MQTT_Client/include/MQTT_Client.hpp
@@ -6,23 +6,26 @@
 #ifndef MQTT_CLIENT_H
 #define MQTT_CLIENT_H
 
-//#include <boost/smart_ptr.hpp>
+#include <boost/smart_ptr.hpp>
 
 namespace MQTT_Client_NS
 {
+	using namespace std;
+	using namespace boost;
+
     class MQTT_Client;
 
     class MQTT_Client
     {
-        //typedef shared_ptr<MQTT_Client>   PMQTT_Client;
-        //typedef weak_ptr<MQTT_Client>     PWMQTT_Client;
+        typedef std::shared_ptr<MQTT_Client*>   PMQTT_Client;
+        typedef std::weak_ptr<MQTT_Client*>     PWMQTT_Client;
 
     public:
         int add(int, int);
 		int mul(int, int);
 
     private:
-        //PMQTT_Client mqtt_ClientPtr_;
+		PMQTT_Client mqtt_ClientPtr_;
     };
 }
 
--- MQTT_Client/src/MQTT_Client.cpp
+++ MQTT_Client/src/MQTT_Client.cpp
@@ -4,13 +4,31 @@
 *
 */
 #include "../include/MQTT_Client.hpp"
+#include <boost/thread.hpp>
+#include <boost/chrono.hpp>
+#include <iostream>
 
+void wait(int seconds)
+{
+	boost::this_thread::sleep_for(boost::chrono::seconds{ seconds });
+}
+
+void mythread()
+{
+	for (int i = 0; i < 5; ++i)
+	{
+		wait(1);
+		std::cout << i << '\n';
+	}
+}
 
 namespace MQTT_Client_NS
 {
 
 	int MQTT_Client::add(int a, int b)
-	{
+		{
+		boost::thread t{ mythread };
+		t.join();
 		return a + b;
 	}
 
