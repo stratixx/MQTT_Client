730cbc0dd92b559718fa717d6aeec3520e94b97c
stratixx <darnokusex@wp.pl>
2019-06-07 18:24:14 +0200

    typedef socket int dla lunixa

--- MQTT_Client/include/SocketHandle.hpp
+++ MQTT_Client/include/SocketHandle.hpp
@@ -7,12 +7,14 @@
 #endif
 #include <winsock2.h>
 #include <Ws2tcpip.h>
+
 #else
 /* Assume that any non-Windows platform uses POSIX-style sockets instead. */
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>  /* Needed for getaddrinfo() and freeaddrinfo() */
 #include <unistd.h> /* Needed for close() */
+typedef int SOCKET;
 #endif
 
 class SocketHandle
